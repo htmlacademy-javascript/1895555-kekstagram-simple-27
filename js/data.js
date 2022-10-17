ec2ffc1db13ec42f3d1ddf03cd80efae5441ded7
Valerii <valerii.sidnev@gmail.com>
2022-10-17 21:31:58 +0700

    DZ-5_v3

--- js/data.js
+++ js/data.js
@@ -1,23 +1,30 @@
 // data.js — модуль, который создаёт данные.
-import {getRandomPositiveInteger} from './util.js';
-import {DESCRIPTIONS, COMMENTS, SIMILAR_DESCRIPTION_PHOTO_COUNT} from './constants.js';
+import {
+  getRandomPositiveInteger
+} from './util.js';
+import {
+  DESCRIPTIONS,
+  SIMILAR_DESCRIPTION_PHOTO_COUNT
+} from './constants.js';
 
 
 //Колбэк рандомного элемента массива
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
 //Сборка объекта
-const getPhotos = (comID) => ({
-  id: comID,
-  url: `photos/${getRandomPositiveInteger(1, 25)}.jpg`,
+const getPhotos = (com) => ({
+  id: com + 1,
+  url: `photos/${com}.jpg`,
   description: getRandomArrayElement(DESCRIPTIONS),
   likes: getRandomPositiveInteger(15, 200),
-  comments: getRandomArrayElement(COMMENTS)
+  comments: getRandomPositiveInteger(0, 200),
 });
 
 // Указываем нужное колличество экземпляров
 const getDescriptionPhoto = () => Array.from({
   length: SIMILAR_DESCRIPTION_PHOTO_COUNT
-}, (v,k) => getPhotos(k));
+}, (v, k) => getPhotos(k));
 
-export{getDescriptionPhoto};
+export {
+  getDescriptionPhoto
+};
--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-import './data.js';
+import {
+  getDescriptionPhoto
+} from './data.js';
+
+// но без console.log {object object} в консоли то нормально не вывести..
+// ТАК ЖЕ, при запуске проекта через npm start НЕ ОТОБРАЖАЕТСЯ в консоли выполненный код.
+
+// eslint-disable-next-line no-console
+console.log(getDescriptionPhoto());
